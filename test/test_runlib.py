ba6f1a78f1ad2ba19fda1f6486394614b4f4e56e
Santiago Torres <torresariass@gmail.com>
2016-05-23 23:42:20 -0400

    ENH: Adds broken tests

---
+++ test/test_runlib.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+    TODO: this
+"""
+
+import unittest
+
+class TestRunlib(unittest.TestCase):
+
+  def test_unimplemented(self):
+    raise self.assertFalse("Missing test implementation!")
+
+# Run unit test.
+if __name__ == '__main__':
+  unittest.main()
