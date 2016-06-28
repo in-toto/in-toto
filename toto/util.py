9d37555ca155286173ae8043231c9bba6c7ef010
Lukas P <luk.puehringer@gmail.com>
2016-06-28 13:53:19 -0400

    Adds throw-away function get_key to utils
    This function uses the crypto module to create an RSA keypair
    and stores the serialized (pickle) dict to a file or reads
    and deserializes the key form file if it already exists
    
    Later this should be replaced by a proper PKI

--- toto/util.py
+++ toto/util.py
@@ -1,7 +1,26 @@
 """
     TODO: markup
 """
-class UnimplementedException(Exception):
-    pass
 
-raise UnimplementedException("This module is yet to be implemented!")
+import os
+import sys
+import pickle
+
+import toto.ssl_crypto.keys
+
+def get_key():
+  """Quick throw-away function that returns a test-key from the test-repo. 
+  If there is no such key in the test-repo, create one."""
+
+  key_dict_fn = "test-repo/test-key.keydict"
+
+  if os.path.isfile(key_dict_fn):
+    with open(key_dict_fn, "r") as key_dict_file:
+      key_dict = pickle.loads(key_dict_file.read())
+
+  else:
+      key_dict = toto.ssl_crypto.keys.generate_rsa_key()
+      with open(key_dict_fn, "w+") as key_dict_file:
+        key_dict_file.write(pickle.dumps(key_dict))
+
+  return key_dict
