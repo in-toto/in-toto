f524c0a82dba043401a04252ba2470b87c045cdd
Lukas P <luk.puehringer@gmail.com>
2016-09-27 15:52:47 -0400

    Deletes supply chain examples

--- toto/models/old_examples/dump.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in archiso debian django gcc tails simple_layout
-do
-    python ${i}.py > $i.layout
-done
-
-a2ps -R -f 6 *.layout -o layouts.ps
-ps2pdf layouts.ps metadata_sketches.pdf
