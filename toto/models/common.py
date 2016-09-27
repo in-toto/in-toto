befe78a29c5481fb82c343a4ab4ece326df125da
Lukas P <luk.puehringer@gmail.com>
2016-09-27 18:20:53 -0400

    common: adds Signable class, removes Metablock
    More generalization can be done later

--- toto/models/common.py
+++ toto/models/common.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
 
-import json
-import attr
-import canonicaljson
+class Signable(object):
+    """ Objects of signable can sign themselves, i.e. their __repr__ 
+    without the signatures property, and store the signature to a signature
+    property.
+    """
 
-@attr.s(repr=False)
-class Metablock(object):
+    signatures = attr.ib(False, repr=False)
 
-    def __repr__(self):
-        return canonicaljson.encode_pretty_printed_json(attr.asdict(self))
-
-    _name=attr.ib(validator=attr.validators.instance_of(str))
+    def sign(self, key):
+        """ Signs the JSON repr of itself (without the signatures property) and 
+        adds the signatures to its signature properties. """
