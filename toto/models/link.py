19139fcfa978d39e95070c786771bae42c278eb6
Lukas P <luk.puehringer@gmail.com>
2016-09-29 12:48:05 -0400

    Adds byproducts to link, simplifies read method

--- toto/models/link.py
+++ toto/models/link.py
@@ -1,37 +1,25 @@
 from common import Signable
 
 @attr.s(repr=False)
-class Link(SignedFile):
+class Link(Signable):
 
-    _type = attr.ib(default="Link", init=False)
-    _name = attr.ib()
-    materials = attr.ib([])
-    products = attr.ib([])
-    ran_command = attr.ib()
-    return_value = attr.ib()
+  _type = attr.ib("Link", init=False)
+  name = attr.ib("")
+  materials = attr.ib({})
+  products = attr.ib({})
+  byproducts = attr.ib({})
+  ran_command = attr.ib("")
+  return_value = attr.ib(None)
 
+  def dump(self, filename):
+    # Magic: short circuiting and string formatting
+    super(Layout, self).dump(filename or "%s.link" % self._name)
 
-    def dump(self, filename):
-        with open(filename, 'wt') as fp:
-            fp.write("{}".format(self))
+  @staticmethod
+  def read_from_file(filename):
+    with open(filename, 'r') as fp:
+      return Link.read(json.load(fp))
 
-    @staticmethod
-    def read(filename):
-        with open(filename, 'r') as fp:
-
-            link = Link()
-            link_data = json.load(fp)
-
-            link._name = link_data._name
-            link.command = link_data.command
-
-            for filepath, filehash in link_data.materials.iteritems():
-                link.materials[filepath] = filehash
-
-            for filepath, filehash in link_data.products.iteritems():
-                link.products[filepath] = filehash
-
-            for key, val in link_data.byproducts.iteritems():
-                link.byproducts[key] = val
-
-            return link
+  @staticmethod
+  def read(data)
+    return Link(**link_data)
