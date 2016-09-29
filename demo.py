8a4bf5c989bc766e799b995a104b053c0820623d
Lukas P <luk.puehringer@gmail.com>
2016-09-29 18:10:50 -0400

    Moves create_and_persist_or_load_key to util.py

--- demo.py
+++ demo.py
@@ -1,27 +1,15 @@
 import os
-import json
 
 import toto.ssl_crypto.keys
 import toto.models.layout as m
+import toto.util
 
-
-def create_and_persist_or_load_key(filename):
-  if not os.path.isfile(filename):
-    key_dict = toto.ssl_crypto.keys.generate_rsa_key()
-    with open(filename, "w+") as fp:
-      key_data = toto.ssl_crypto.keys.format_keyval_to_metadata(
-          key_dict["keytype"], key_dict["keyval"], private=True)
-      fp.write(json.dumps(key_data))
-      return key_dict
-  else:
-    with open(filename, "r") as fp:
-      return toto.ssl_crypto.keys.format_metadata_to_key(json.load(fp))
 
 def main():
 
   # Get keys
-  alice_key = create_and_persist_or_load_key("alice")
-  bob_key = create_and_persist_or_load_key("bob")
+  alice_key = toto.util.create_and_persist_or_load_key("alice")
+  bob_key = toto.util.create_and_persist_or_load_key("bob")
 
 
   # Create Layout
--- toto/util.py
+++ toto/util.py
@@ -1,26 +1,24 @@
 """
     TODO: markup
 """
-
 import os
 import sys
 import pickle
+import json
 
 import toto.ssl_crypto.keys
 
-def get_key():
-  """Quick throw-away function that returns a test-key from the test-repo. 
-  If there is no such key in the test-repo, create one."""
+def create_and_persist_or_load_key(filename):
+  """Throw-away function that returns loads a key file or creates and
+  stores it if it does not expist."""
 
-  key_dict_fn = "test-repo/test-key.keydict"
-
-  if os.path.isfile(key_dict_fn):
-    with open(key_dict_fn, "r") as key_dict_file:
-      key_dict = pickle.loads(key_dict_file.read())
-
+  if not os.path.isfile(filename):
+    key_dict = toto.ssl_crypto.keys.generate_rsa_key()
+    with open(filename, "w+") as fp:
+      key_data = toto.ssl_crypto.keys.format_keyval_to_metadata(
+          key_dict["keytype"], key_dict["keyval"], private=True)
+      fp.write(json.dumps(key_data))
+      return key_dict
   else:
-      key_dict = toto.ssl_crypto.keys.generate_rsa_key()
-      with open(key_dict_fn, "w+") as key_dict_file:
-        key_dict_file.write(pickle.dumps(key_dict))
-
-  return key_dict
+    with open(filename, "r") as fp:
+      return toto.ssl_crypto.keys.format_metadata_to_key(json.load(fp))
