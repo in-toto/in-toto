da9d5e843248d8a3d12dea8a0a2c76166f39c5bd
Lukas P <luk.puehringer@gmail.com>
2016-10-04 19:47:35 -0400

    Adds logging to runlib

--- toto/runlib.py
+++ toto/runlib.py
@@ -171,9 +171,16 @@ def run_link(name, materials, products, toto_cmd_args, record_byproducts=False):
   XXX: This should probably be atomic, i.e. all or nothing"""
 
   # Record - Run - Record
+  log.doing("record materials for link '%s'" % name)
   materials_dict = record_artifacts_as_dict(materials)
+
+  log.doing("run command '%s' for link '%s'" % (toto_cmd_args, name))
   byproducts, return_value = execute_link(toto_cmd_args, record_byproducts)
+
+  log.doing("record products for link '%s'" % name)
   products_dict = record_artifacts_as_dict(products)
+
+  log.doing("create link metadata for link '%s'" % name)
 
   link = create_link_metadata(name, materials_dict, products_dict, byproducts,
     toto_cmd_args, return_value)
@@ -187,6 +194,10 @@ def toto_run(name, materials, products, key, toto_cmd_args,
         record_byproducts)
 
   # XXX LP: Change key load
+  log.doing("load key '%s'" % key)
   key_dict = toto.util.create_and_persist_or_load_key(key)
+
+  log.doing("sign link metadata '%s' with key '%s'" % (name, key))
   link.sign(key_dict)
+  log.doing("store link metadata '%s' to disk" % key)
   link.dump()
