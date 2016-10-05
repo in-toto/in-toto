a4410ccf7769ed1a00872d2380bea63c98adbaa5
Lukas P <luk.puehringer@gmail.com>
2016-10-04 20:20:18 -0400

    Changes log passing and failing to logging.CRITICAL

--- toto/log.py
+++ toto/log.py
@@ -6,11 +6,13 @@ logging.basicConfig(level=settings.LOG_LEVEL, format='%(message)s')
 def doing(msg):
   logging.info("DOING:   %s" % str(msg))
 
-def passing(msg):
-  logging.info("PASSING: %s" % str(msg))
-
 def warning(msg):
   logging.warn("WARNING: %s" % str(msg))
 
+def passing(msg):
+  logging.critical("PASSING: %s" % str(msg))
+
 def failing(msg):
-  logging.warn("FAILING: %s" % str(msg))
+  logging.critical("FAILING: %s" % str(msg))
+
+error = logging.error
