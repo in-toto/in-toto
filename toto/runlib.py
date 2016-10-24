4233be10e74824a12ef45e07a27f00b98a653d8e
Lukas P <luk.puehringer@gmail.com>
2016-10-24 17:47:29 -0400

    Enhances libyfication in runlib
    - Un-nestes functions
    - Moves toto_run to toto-run.py where it calls into runlib
      functions and controls the output and return values
    - Changes toto-run.py --name option to --step-name
    - Adds proper library function docstrings
    - Adapts demo.py

--- toto/runlib.py
+++ toto/runlib.py
@@ -35,53 +35,63 @@ if os.name == 'posix' and sys.version_info[0] < 3:
 else:
   import subprocess
 
-# XXX LP: I think we'll get rid of toto formats because we want to use
-# model validators instead for toto schemas, we'll still use
-# toto.ssl_crypto.formats though
-
-# import toto.formats
-
 import toto.models.link
 import toto.log as log
 
 import toto.ssl_crypto.hash
 import toto.ssl_crypto.formats
 
+def _hash_artifact(filepath, hash_algorithms=['sha256']):
+  """Internal helper that takes a filename and hashes the respective file's
+  contents using the passed hash_algorithms and returns a hashdict conformant
+  with ssl_crypto.formats.HASHDICT_SCHEMA. """
+  toto.ssl_crypto.formats.HASHALGORITHMS_SCHEMA.check_match(hash_algorithms)
+  hash_dict = {}
+
+  for algorithm in hash_algorithms:
+    digest_object = toto.ssl_crypto.hash.digest_filename(filepath, algorithm)
+    hash_dict.update({algorithm: digest_object.hexdigest()})
+
+  toto.ssl_crypto.formats.HASHDICT_SCHEMA.check_match(hash_dict)
+
+  return hash_dict
+
+
+def _normalize_path(path):
+  """Internal helper that strips "./" on the left side of the path. """
+  if path.startswith("./"):
+      return path[2:]
+  return path
+
 
 def record_artifacts_as_dict(artifacts):
-  """Takes a list of directories and/or filenames as input and creates a dict
-  with filepaths as keys and their files' hashes as values.
+  """
+  <Purpose>
+    Hashes each file in the passed path list. If the path list contains
+    paths to directories the directory tree(s) are traversed.
 
-  The dirs/files a link command is executed on are called materials.
-  The dirs/files that result form a link command execution are called
-  products."""
+    The files a link command is executed on are called materials.
+    The files that result form a link command execution are called
+    products.
 
+  <Arguments>
+    artifacts:
+            A list of file or directory paths used as materials or products for
+            the link command.
+
+  <Exceptions>
+    TBA (see https://github.com/in-toto/in-toto/issues/6)
+
+  <Side Effects>
+    Calls functions to generate cryptographic hashes.
+
+  <Returns>
+    A dictionary with file paths as keys and the files' hashes as values.
+  """
   artifacts_dict = {}
 
   if not artifacts:
     return artifacts_dict
-
-  def _hash_artifact(filepath, hash_algorithms=['sha256']):
-    """Takes a filename and hashes the respective file's contents
-    using the passed hash_algorithms. Returns a HASHDICT"""
-
-    toto.ssl_crypto.formats.HASHALGORITHMS_SCHEMA.check_match(hash_algorithms)
-    hash_dict = {}
-
-    for algorithm in hash_algorithms:
-      digest_object = toto.ssl_crypto.hash.digest_filename(filepath, algorithm)
-      hash_dict.update({algorithm: digest_object.hexdigest()})
-
-    toto.ssl_crypto.formats.HASHDICT_SCHEMA.check_match(hash_dict)
-
-    return hash_dict
-
-  def _normalize_path(path):
-    """Strips "./" on the left side of the path"""
-
-    if path.startswith("./"):
-        return path[2:]
-    return path  # or whatever
 
   for artifact in artifacts:
     if os.path.isfile(artifact):
@@ -96,11 +106,35 @@ def record_artifacts_as_dict(artifacts):
 
 
 def execute_link(link_cmd_args, record_byproducts):
-  """Takes a command and its options and arguments of the software supply
-  chain as input, runs the command in a subprocess, records stdout,
-  stderr and return value of the command and returns them. Stdout and stderr
-  are called by-products."""
+  """
+  <Purpose>
+    Executes the passed command plus arguments in a subprocess and returns
+    the return value of the executed command. If the specified standard output
+    and standard error of the command are recorded and also returned to the
+    caller.
 
+  <Arguments>
+    link_cmd_args:
+            A list where the first element is a command and the remaining
+            elements are arguments passed to that command.
+    record_byproducts:
+            A bool that specifies whether to redirect standard output and
+            and standard error to a temporary file which is returned to the
+            caller (True) or not (False).
+
+  <Exceptions>
+    TBA (see https://github.com/in-toto/in-toto/issues/6)
+
+  <Side Effects>
+    Executes passed command in a subprocess and redirects stdout and stderr
+    if specified.
+
+  <Returns>
+    - A dictionary containg standard output and standard error of the
+      executed command, called by-products.
+      Note: If record_byproducts is False, the dict values are empty strings.
+    - The return value of the executed command.
+  """
   # XXX: The first approach only redirects the stdout/stderr to a tempfile
   # but we actually want to duplicate it, ideas
   #  - Using a pipe won't work because processes like vi will complain
@@ -132,51 +166,96 @@ def execute_link(link_cmd_args, record_byproducts):
 
   return {"stdout": stdout_str, "stderr": stderr_str}, return_value
 
-def create_link_metadata(name, materials, products, byproducts,
-    command, return_value):
-  """Takes the state of the materials (before link command execution), the state
-  of the products (after link command execution) and the by-products of the link
-  command execution and creates and returns a Link metadata object """
+def create_link_metadata(link_name, materials_dict, products_dict,
+    link_cmd_args, byproducts, return_value):
+  """
+  <Purpose>
+    Takes the state of the materials (before link command execution), the state
+    of the products (after link command execution) and the by-products and
+    return value of the link command execution and creates and returns a
+    Link metadata object.
 
+  <Arguments>
+    link_name:
+            A unique name to relate link metadata with a step defined in the
+            layout.
+    materials_dict:
+            A dictionary with file paths as keys and the files' hashes as
+            values.
+    products_dict:
+            A dictionary with file paths as keys and the files' hashes as
+            values.
+    link_cmd_args:
+            A list where the first element is a command and the remaining
+            elements are arguments passed to that command.
+    byproducts:
+            A dictionary in the format containing standard output and standard
+            error of the executed link command, i.e.:
+            {"stdout": "<standard output", "stderr": "<standard error>"}
+    return_value:
+            The return value of the executed command.
+
+  <Exceptions>
+    TBA (see https://github.com/in-toto/in-toto/issues/6)
+
+  <Side Effects>
+    Creates a Link object from a Python dictionary.
+
+  <Returns>
+    - A Link metadata object
+  """
   link_dict = {
-    "name" : name,
-    "materials" : materials,
-    "products" : products,
+    "name" : link_name,
+    "materials" : materials_dict,
+    "products" : products_dict,
+    "command" : link_cmd_args,
     "byproducts" : byproducts,
-    "command" : command,
     "return_value" : return_value
   }
-
   return  toto.models.link.Link.read(link_dict)
 
 
-def run_link(name, materials, products, toto_cmd_args, record_byproducts=False):
-  """Performs all actions associated with toto run-link.
-  XXX LP: This should probably be atomic, i.e. all or nothing"""
-
-  # log.doing("record materials for '%s'" % name)
-  materials_dict = record_artifacts_as_dict(materials)
-
-  # log.doing("run command '%s' for '%s'" % (toto_cmd_args, name))
-  byproducts, return_value = execute_link(toto_cmd_args, record_byproducts)
-
-  # log.doing("record products for '%s'" % name)
-  products_dict = record_artifacts_as_dict(products)
-
-  # log.doing("create metadata for '%s'" % name)
-  link = create_link_metadata(name, materials_dict, products_dict, byproducts,
-    toto_cmd_args, return_value)
-
-  return link
-
-def toto_run(name, materials, products, key, toto_cmd_args,
+def run_link(link_name, materials_list, products_list, link_cmd_args,
     record_byproducts=False):
-  """Calls run link signs the resulting link metadata and stores it to a file """
+  """
+  <Purpose>
+    Wrapper to record materials, execute a link, record products and create
+    and return a Link metadata object.
 
-  link = toto.runlib.run_link(name, materials, products, toto_cmd_args,
-        record_byproducts)
+  <Arguments>
+    link_name:
+            A unique name to relate link metadata with a step defined in the
+            layout.
+    materials_list:
+            A list of file or directory paths used as materials for
+            the link command.
+    products_list:
+            A list of file or directory paths used as materials for
+            the link command.
+    link_cmd_args:
+            A list where the first element is a command and the remaining
+            elements are arguments passed to that command.
+    record_byproducts:
+            A bool that specifies whether to redirect standard output and
+            and standard error to a temporary file which is returned to the
+            caller (True) or not (False).
 
-  # log.doing("sign metadata '%s' with key '%s'" % (name, key))
-  link.sign(key)
-  # log.doing("store metadata '%s' to disk" % name)
-  link.dump()
+  <Exceptions>
+    TBA (see https://github.com/in-toto/in-toto/issues/6)
+
+  <Side Effects>
+    - Calls function to record materials.
+    - Calls function to execute link command.
+    - Calls function to record products.
+    - Calls function to create Link object.
+
+  <Returns>
+    A Link metadata object
+  """
+  materials_dict = record_artifacts_as_dict(materials_list)
+  byproducts, return_value = execute_link(link_cmd_args, record_byproducts)
+  products_dict = record_artifacts_as_dict(products_list)
+  return create_link_metadata(link_name, materials_dict, products_dict,
+      link_cmd_args, byproducts, return_value)
+
+
