3697e8b19a08ffb1537835f606a5cfedbeaa7244
Lukas P <luk.puehringer@gmail.com>
2016-10-26 13:35:12 -0400

    Makes RSA password optional
    RSA private keys can now be created and loaded without
    password.
    Utils figures out whether a password is required when
    a key is loaded and only then shows a prompt to the user.
    This required some changes to the ssl_crypto submodule
    554af0816b4d23eb0e2f8d1bbdabf84372fa3890

--- toto/util.py
+++ toto/util.py
@@ -6,23 +6,27 @@ import getpass
 
 import toto.ssl_crypto.keys
 import toto.ssl_crypto.formats
+import toto.ssl_commons.exceptions
 import toto.log as log
 
 
-def generate_and_write_rsa_keypair(filepath, password):
+def generate_and_write_rsa_keypair(filepath, password=None):
   """
   <Purpose>
-    Generate an RSA key file, create an encrypted PEM string (using 'password'
-    as the pass phrase), and store it in 'filepath'.  The public key portion of
-    the generated RSA key is stored in <'filepath'>.pub
+    Generate an RSA key keypair and store public and private portion each
+    to a file in PEM format.
+    If a password is specified the private key is encrypted using that password
+    as pass phrase.
+    The private key is stored to <'filepath'> and the public key to
+    <'filepath'>.pub
 
   <Arguments>
     filepath:
       The public and private key files are saved to <filepath>.pub, <filepath>
       respectively
 
-    password:
-      The password used to encrypt 'filepath'
+    password: (optional)
+      If specified the password is used to encrypt the private key
 
   <Exceptions>
     ssl_commons.FormatError, if the arguments are improperly formatted
@@ -33,15 +37,18 @@ def generate_and_write_rsa_keypair(filepath, password):
   <Returns>
     None.
   """
-
   toto.ssl_crypto.formats.PATH_SCHEMA.check_match(filepath)
-  toto.ssl_crypto.formats.PASSWORD_SCHEMA.check_match(password)
 
   rsa_key = toto.ssl_crypto.keys.generate_rsa_key()
 
   public = rsa_key['keyval']['public']
   private = rsa_key['keyval']['private']
-  private_pem = toto.ssl_crypto.keys.create_rsa_encrypted_pem(private, password)
+
+  if password:
+    private_pem = toto.ssl_crypto.keys.create_rsa_encrypted_pem(private,
+        password)
+  else:
+    private_pem = private
 
   with open(filepath + '.pub', 'w') as fo_public:
     fo_public.write(public.encode('utf-8'))
@@ -53,16 +60,17 @@ def generate_and_write_rsa_keypair(filepath, password):
 def import_rsa_key_from_file(filepath, password=None):
   """
   <Purpose>
-    Import the RSA key stored in PEM format to 'filepath'. This could be
+    Import the RSA key stored in PEM format to 'filepath'. This can be
     a public key or a private key.
-    If a password is specified, the key will be decrypted
+    If it is a private key and the password is specified, it will be used
+    to decrypt the private key.
 
   <Arguments>
     filepath:
       <filepath> file, an RSA PEM file
 
-    password (optional):
-      if a password is specified, the imported key will be decrypted
+    password: (optional)
+      If a password is specified, the imported private key will be decrypted
 
   <Exceptions>
     ssl_commons.FormatError, if the arguments are improperly formatted
@@ -73,30 +81,36 @@ def import_rsa_key_from_file(filepath, password=None):
   <Returns>
     An RSA key object conformant to 'tuf.formats.RSAKEY_SCHEMA'
   """
-
   toto.ssl_crypto.formats.PATH_SCHEMA.check_match(filepath)
 
   with open(filepath, 'rb') as fo_pem:
     rsa_pem = fo_pem.read().decode('utf-8')
 
-  if password:
-    toto.ssl_crypto.formats.PASSWORD_SCHEMA.check_match(password)
-    rsa_key = toto.ssl_crypto.keys.import_rsakey_from_encrypted_pem(rsa_pem,
-        password)
-  else:
+  if toto.ssl_crypto.keys.is_pem_private(rsa_pem):
+    rsa_key = toto.ssl_crypto.keys.import_rsakey_from_pem(rsa_pem, password)
+
+  elif toto.ssl_crypto.keys.is_pem_public(rsa_pem):
     rsa_key = toto.ssl_crypto.keys.format_rsakey_from_pem(rsa_pem)
+  else:
+    raise toto.ssl_commons.exceptions.FormatError('The key has to be either'
+            'a private or public RSA key in PEM format')
 
   return rsa_key
 
 
 def prompt_password(prompt="Enter password: "):
-  """Prompts for password input and returns the password"""
+  """Prompts for password input and returns the password. """
   return getpass.getpass(prompt, sys.stderr)
 
 
 def prompt_import_rsa_key_from_file(filepath):
-  """Prompts for password and calls import_rsa_key_from_file"""
-  password = prompt_password()
+  """Trys to load the key without password. If a CryptoError occurs, prompts
+  the user for a password and trys to load the the key again. """
+  password = None
+  try:
+    import_rsa_key_from_file(filepath)
+  except toto.ssl_commons.exceptions.CryptoError, e:
+    password = prompt_password()
   return import_rsa_key_from_file(filepath, password)
 
 
