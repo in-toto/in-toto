8f9f69be372b8aeb028548a8f2015169e3056f88
Lukas P <luk.puehringer@gmail.com>
2016-10-28 18:58:16 -0400

    Removes unneeded plus from multiline string

--- toto/runlib.py
+++ toto/runlib.py
@@ -37,7 +37,7 @@ if os.name == 'posix' and sys.version_info[0] < 3:
     import subprocess32 as subprocess
   except Exception, e:
     logging.warning("POSIX users (Linux, BSD, etc.) are strongly encouraged to"
-        + " install and use the much more recent subprocess32")
+        " install and use the much more recent subprocess32")
     import subprocess
 else:
   import subprocess
