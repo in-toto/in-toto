d91f357b8c8074bd8d92cb24d2c857606a630fa2
Santiago Torres <torresariass@gmail.com>
2016-10-31 15:03:07 -0400

    FIX: setup.py: Addresses comments by lukas
    This refers to:
    
        - Update the requirements (was missing python-dateutil and iso8601)
    
        - Update the name of simple-settings (was simple_settings)
    
        - Update the indentation to match the coding guidelines (was 4)

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 """
-    TODO: this
+  TODO: this
 """
 from setuptools import setup, find_packages
 
 setup(
-    name="toto-framework",
-    version="0.0.1",
-    author="New York University: Secure Systems Lab",
-    author_email="santiago@nyu.edu",
-    description=("A framework to define and secure "
-                 "the integrity of software supply chains"),
-    license="MIT",
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    install_requires=['six', 'simple_settings', 'attrs', 'canonicaljson'],
-    test_suite="test.runtests",
-    scripts=['toto/toto-run.py',
-             'toto/toto-verify.py']
+  name="toto-framework",
+  version="0.0.1",
+  author="New York University: Secure Systems Lab",
+  author_email="santiago@nyu.edu",
+  description=("A framework to define and secure "
+               "the integrity of software supply chains"),
+  license="MIT",
+  packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+  install_requires=['six', 'simple-settings', 'attrs', 'canonicaljson',
+                    'python-dateutil', 'iso8601'],
+  test_suite="test.runtests",
+  scripts=['toto/toto-run.py',
+           'toto/toto-verify.py']
 )
