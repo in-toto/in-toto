7e57b5b5cff0691f795df11da48b9295513c703f
Lukas P <luk.puehringer@gmail.com>
2016-11-21 18:49:21 -0500

    Adds toto_start_record and toto_stop_record (WIP)
    In some cases it is not feasible to wrap a single command with
    toto-run, e.g. when allowedly editing a bunch of files shortly before
    building.
    To not disrupt the in-toto supply chain we provide two additional cli
    tools that, where one only records materials to a temporary file
    and the second records products and finalizes the link metadata
    file.

--- toto/toto-run.py
+++ toto/toto-run.py
@@ -35,10 +35,73 @@ import argparse
 import toto.util
 import toto.runlib
 import toto.log as log
+from toto.models.link import Link
 
 def _die(msg, exitcode=1):
   log.error(msg)
   sys.exit(exitcode)
+
+
+def in_toto_start_record(step_name, key_path, material_list):
+  """Load link signing private keys from disk, record passed materials,
+  sign with key, store to disk as:
+  .<step_name>.link-unfinished
+
+  """
+  try:
+    log.doing("Start recording '{0}'...".format(step_name))
+    key = toto.util.prompt_import_rsa_key_from_file(key_path)
+
+    log.doing("record materials...")
+    materials_dict = toto.runlib.record_artifacts_as_dict(material_list)
+
+    log.doing("create preliminary link metadata...")
+    link = toto.runlib.create_link_metadata(step_name, materials_dict)
+
+    log.doing("sign metadata...")
+    link.sign(key)
+
+    log.doing("store metadata to '.{0}.link-unfinished'...".format(step_name))
+    link.dump()
+
+  except Exception, e:
+    _die("in start record - %s" % e)
+
+
+def in_toto_stop_record(step_name, key_path, product_list):
+  """Load key, load .<step_name>.link-unfinished exists
+  if exists, verify signature, record products, sign, dump, remove
+  link-unfinished.
+
+  """
+  unfinished_fn = "." + str(step_name) + ".link-unfinished"
+  try:
+    log.doing("load link signing key...")
+    key = toto.util.prompt_import_rsa_key_from_file(key_path)
+
+    # There must be a file .<step_name>.link-unfinished in the current dir
+    log.doing("load unfinished link file...")
+    link_unfinished = Link.read_from_file(unfinished_fn)
+
+    # The file must have been signed by the same key
+    log.doing("verify unfinished link signature...")
+    link_unfinished.verify_signatures(key)
+
+    log.doing("record products...")
+    link_unfinished.products = toto.runlib.record_artifacts_as_dict(product_list)
+
+    log.doing("update signature...")
+    link_unfinished.signatures = []
+    link_unfinished.sign(key)
+
+    log.doing("store metadata to file...")
+    link.dump()
+
+    log.doing("remove unfinished file...")
+    os.remove(unfinished_fn)
+
+  except Exception, e:
+    _die("in stop record - %s" % e)
 
 def in_toto_run(step_name, key_path, material_list, product_list,
     link_cmd_args, record_byproducts=False):
@@ -91,18 +154,27 @@ def in_toto_run(step_name, key_path, material_list, product_list,
 
 def main():
   parser = argparse.ArgumentParser(
-      description="Executes link command and records metadata",
-      usage="toto-run.py --step-name <unique step name>\n" +
-            "                     [--materials <filepath>[ <filepath> ...]]\n" +
-            "                     [--products <filepath>[ <filepath> ...]]\n" +
-            "                      --key <functionary private key path>\n" +
-            "                     [--record-byproducts]\n" +
-            "                      -- <cmd> [args]")
+      description="Executes link command and records metadata")
+  parser.usage = ("\n"
+      "%(prog)s  --step-name <unique step name>\n{0}"
+               " --key <functionary private key path>\n{0}"
+               "[--materials <filepath>[ <filepath> ...]]\n{0}"
+               "[--products <filepath>[ <filepath> ...]]\n{0}"
+               "[--record-byproducts] -- <cmd> [args]\n\n"
+
+      "%(prog)s  --start-record --step-name <unique step name>\n{0}"
+               " --key <functionary private key path>\n{0}"
+               "[--materials <filepath>[ <filepath> ...]]\n\n"
+
+      "%(prog)s --stop-record --step-name <unique step name>\n{0}"
+               "--key <functionary private key path>\n{0}"
+               "[--materials <filepath>[ <filepath> ...]]"
+                .format((len(parser.prog) + 1) * " "))
 
   toto_args = parser.add_argument_group("Toto options")
-  # XXX LP: Name has to be unique!!! Where will we check this?
-  # XXX LP: Do we limit the allowed characters for the name?
-  # XXX LP: Should it be possible to add a path?
+  # FIXME: Name has to be unique!!! Where will we check this?
+  # FIXME: Do we limit the allowed characters for the name?
+  # FIXME: Should it be possible to add a path?
   toto_args.add_argument("-n", "--step-name", type=str, required=True,
       help="Unique name for link metadata")
 
@@ -111,7 +183,7 @@ def main():
   toto_args.add_argument("-p", "--products", type=str, required=False,
       nargs='+', help="Files to record after link command execution")
 
-  # XXX LP: Specifiy a format or choice of formats to use
+  # FIXME: Specifiy a format or choice of formats to use
   toto_args.add_argument("-k", "--key", type=str, required=True,
       help="Path to private key to sign link metadata")
 
@@ -120,8 +192,8 @@ def main():
       default=False, action='store_true')
 
   link_args = parser.add_argument_group("Link command")
-  # XXX: This is not yet ideal.
-  # What should we do with tokens like > or ;
+  # FIXME: This is not yet ideal.
+  # What should we do with tokens like > or ; ?
   link_args.add_argument("link_cmd", nargs="+",
     help="Link command to be executed with options and arguments")
 
