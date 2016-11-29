192bb35abd1b57521a5579ca57d123cce03d5448
Lukas P <luk.puehringer@gmail.com>
2016-11-29 15:35:38 -0500

    Adds factories, removes util classes
    - Adds default factories for lists and dictionaries to in-toto model
    attributes. This fixes a strange python/attrs behavior that
    re-uses previously allocated memory space for e.g. lists when initializing
    them with an empty default list:
    ```
    import attr
    @attr.s(repr=False)
    class Foo(object):
      bar = attr.ib([])
    
    Foo().bar.append("foobar")
    Foo().bar  == ["foobar"]
    ```
    
    - Removes obsolete util classes
      GenericPathList
      ComparableHashDict
    
    - Fixes layout.keys default type
      c.f. https://github.com/in-toto/in-toto/issues/41

--- toto/models/common.py
+++ toto/models/common.py
@@ -22,8 +22,6 @@
   Signable:
       sign self, store signature to self and verify signatures
 
-  ComparableHashDict: (helper class)
-      compare contained dictionary of hashes using "=", "!="
 """
 
 import attr
@@ -74,7 +72,7 @@ class Signable(Metablock):
   """Objects with base class Signable can sign their payload (a canonical
   pretty printed JSON string not containing the signatures attribute) and store
   the signature (signature format: ssl_crypto__formats.SIGNATURE_SCHEMA) """
-  signatures = attr.ib([])
+  signatures = attr.ib(default=attr.Factory(list))
 
   @property
   def payload(self):
@@ -107,45 +105,3 @@ class Signable(Metablock):
       if not keys.verify_signature(key, signature, self.payload):
         raise SignatureVerificationError("Invalid signature")
 
-@attr.s(repr=False, cmp=False)
-class ComparableHashDict(object):
-  """Helper class providing that wraps hash dicts (format:
-  toto.ssl_crypto.formats.HASHDICT_SCHEMA) in order to compare them using
-  `=` and `!=`"""
-
-  hash_dict = attr.ib({})
-
-  def __eq__(self, other):
-    """Equal if the dicts have the same keys and the according values
-    (strings) are equal"""
-
-    if self.hash_dict.keys() != other.hash_dict.keys():
-      return False
-
-    for key in self.hash_dict.keys():
-      if self.hash_dict[key] != other.hash_dict[key]:
-        return False
-    return True
-
-  def __ne__(self, other):
-    return not self.__eq__(other)
-
-
-# @attr.s(repr=False)
-# class GenericPathList(object):
-#   """ Helper class implementing __contains__ to provide <path> in <path list>
-#   where <path> can start with "./" or not
-#   """
-#   path_list = attr.ib([])
-
-#   def __contains__(self, item):
-
-#     if item.startswith("./"):
-#       other_item = item.lstrip("./")
-#     else:
-#       other_item = "./" + item
-
-#     if item in self.path_list or \
-#         other_item in self.path_list:
-#       return True
-#     return False
