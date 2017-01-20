f3e561d70a9adfd23c1377fe9c4f76dacc60e8f5
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-01-20 14:51:55 -0500

    Remove securesystemslib settings from settings.py
    This commit removes securesystemslib specific settings from the
    global settings file.
    
    Before securesystemslib became a stand-alone package it used to
    import settings from simple_settings, a package that provided a
    single global settings file.
    simple_settings support was dropped and securesystemslib went back
    to use its own local settings file.

--- in_toto/settings.py
+++ in_toto/settings.py
@@ -27,43 +27,7 @@
   from simple_settings import settings
 
 """
-
 import logging
-
-# Set a directory that should be used for all temporary files. If this
-# is None, then the system default will be used. The system default
-# will also be used if a directory path set here is invalid or
-# unusable.
-temporary_directory = None
-
-# The current "good enough" number of PBKDF2 passphrase iterations.
-# We recommend that important keys, such as root, be kept offline.
-# 'toto.settings.PBKDF2_ITERATIONS' should increase as CPU speeds increase, set here
-# at 100,000 iterations by default (in 2013).  The repository maintainer may opt
-# to modify the default setting according to their security needs and
-# computational restrictions.  A strong user password is still important.
-# Modifying the number of iterations will result in a new derived key+PBDKF2
-# combination if the key is loaded and re-saved, overriding any previous
-# iteration setting used in the old '<keyid>' key file.
-# https://en.wikipedia.org/wiki/PBKDF2
-PBKDF2_ITERATIONS = 100000
-
-# Supported cryptography libraries that can be used to generate and verify RSA
-# keys and signatures:  ['pycrypto', 'pyca-cryptography']
-RSA_CRYPTO_LIBRARY = 'pyca-cryptography'
-
-# Supported Ed25519 cryptography libraries: ['pynacl', 'ed25519']
-ED25519_CRYPTO_LIBRARY = 'ed25519'
-
-# General purpose cryptography. Algorithms and functions that fall under
-# general purpose include AES, PBKDF2, cryptographically strong random number
-# generators, and cryptographic hash functions.  The majority of the general
-# cryptography is needed by the repository and developer tools.
-# RSA_CRYPTO_LIBRARY and ED25519_CRYPTO_LIBRARY are needed on the client side
-# of the software updater.
-# Supported libraries for general-purpose cryptography:  ['pycrypto',
-# 'pyca-cryptography']
-GENERAL_CRYPTO_LIBRARY = 'pycrypto'
 
 # FIXME: Add as command line argument or to config file, e.g .in-toto-ignore
 ARTIFACT_EXCLUDES=["*.link*", ".git", "*.pyc", "*~"]
