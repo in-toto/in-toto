8e4a6ccf18d79fd8b836b3d603fd85a30acc0c22
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-09-01 12:45:48 -0400

    Add header block to runtests.py

--- test/runtests.py
+++ test/runtests.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 """
-    TODO: this documentation!
+<Program Name>
+  runtests.py
+
+<Author>
+  Santiago Torres <santiago@nyu.edu>
+  Lukas Puehringer <lukas.puehringer@nyu.edu>
+
+<Started>
+  May 23, 2016
+
+<Copyright>
+  See LICENSE for licensing information.
+
+<Purpose>
+  Script to search, load and run in-toto tests using the Python `unittest`
+  framework.
 """
 
 from unittest import defaultTestLoader, TextTestRunner
