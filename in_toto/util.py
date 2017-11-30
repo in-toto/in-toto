429af14f9a21686a2c6ecd8a9f69ef5a8eca4b7d
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-11-30 11:16:33 -0500

    Add util function to load public gpg keys as dict
    This function can be used to e.g. add public keys to an in-toto
    layout (trust model) or to load the layout signature verification
    keys when running in-toto-verify.

--- in_toto/util.py
+++ in_toto/util.py
@@ -1,6 +1,7 @@
 import sys
 import getpass
 
+import in_toto.gpg.functions
 import securesystemslib.formats
 import securesystemslib.hash
 import securesystemslib.keys
@@ -100,6 +101,7 @@ def import_rsa_key_from_file(filepath, password=None):
 
   return rsa_key
 
+
 def import_rsa_public_keys_from_files_as_dict(filepaths):
   """Takes a list of filepaths to RSA public keys and returns them as a
   dictionary conformant with securesystemslib.formats.KEYDICT_SCHEMA."""
@@ -110,6 +112,21 @@ def import_rsa_public_keys_from_files_as_dict(filepaths):
     keyid = key["keyid"]
     key_dict[keyid] = key
   return key_dict
+
+
+def import_gpg_public_keys_from_keyring_as_dict(keyids, gpg_home=False):
+  """Creates a dictionary of gpg public keys retrieving gpg public keys
+  identified by the list of passed `keyids` from the gpg keyring at `gpg_home`.
+  If `gpg_home` is False the default keyring is used. """
+  key_dict = {}
+  for gpg_keyid in keyids:
+    pub_key = in_toto.gpg.functions.gpg_export_pubkey(gpg_keyid,
+        homedir=gpg_home)
+    in_toto.gpg.formats.PUBKEY_SCHEMA.check_match(pub_key)
+    keyid = pub_key["keyid"]
+    key_dict[keyid] = pub_key
+  return key_dict
+
 
 def prompt_password(prompt="Enter password: "):
   """Prompts for password input and returns the password. """
