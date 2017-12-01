821032e3277825a416df71e4f3481c601b5318c9
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-12-01 16:45:57 -0500

    Add in-toto-verify tests for gpg signed supply chain
    This commit copies the demo files used for testing where the
    demo layout contains gpg public keys and the link files are signed
    with the corresponding gpg private keys.

--- test/test_in_toto_verify.py
+++ test/test_in_toto_verify.py
@@ -154,5 +154,60 @@ class TestInTotoVerifyTool(unittest.TestCase):
     with self.assertRaises(SystemExit):
       in_toto_verify("wrong-layout-path", [self.alice_path], None, None)
 
+
+class TestInTotoVerifyToolGPG(unittest.TestCase):
+  """ Tests in-toto-verify like TestInTotoVerifyTool but with
+  gpg project owner and functionary keys. """
+
+  @classmethod
+  def setUpClass(self):
+    """Copy test gpg rsa keyring, gpg demo metadata files and demo final
+    product to tmp test dir. """
+
+    self.working_dir = os.getcwd()
+    self.test_dir = os.path.realpath(tempfile.mkdtemp())
+
+    # Copy gpg keyring
+    gpg_keyring_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "gpg_keyrings", "rsa")
+
+    self.gnupg_home = os.path.join(self.test_dir, "rsa")
+    shutil.copytree(gpg_keyring_path, self.gnupg_home)
+
+    self.owner_gpg_keyid = "8465a1e2e0fb2b40adb2478e18fb3f537e0c8a17"
+
+    # Copy gpg demo metadata files
+    demo_files = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "demo_files_gpg")
+    for file in os.listdir(demo_files):
+      shutil.copy(os.path.join(demo_files, file), self.test_dir)
+
+    # Change into test dir
+    os.chdir(self.test_dir)
+
+    # Sign layout template with gpg key
+    layout_template = Metablock.load("demo.layout.template")
+
+    self.layout_path = "gpg_signed.layout"
+    layout_template.sign_gpg(self.owner_gpg_keyid, self.gnupg_home)
+    layout_template.dump(self.layout_path)
+
+
+  @classmethod
+  def tearDownClass(self):
+    """Change back to initial working dir and remove temp dir. """
+    os.chdir(self.working_dir)
+    shutil.rmtree(self.test_dir)
+
+  def test_gpg_signed_layout_with_gpg_functionary_keys(self):
+    """ Successfully test demo supply chain where the layout lists gpg keys
+    as functionary keys and is signed with a gpg key. """
+    args = [ "in-toto-verify", "--layout", self.layout_path,
+            "--gpg", self.owner_gpg_keyid, "--gpg-home", self.gnupg_home]
+
+    with patch.object(sys, 'argv', args):
+      in_toto_verify_main()
+
+
 if __name__ == "__main__":
   unittest.main(buffer=True)
