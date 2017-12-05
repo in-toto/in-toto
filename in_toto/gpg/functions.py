94a5567d728594d0245030608a55a5e11bdfec0c
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-12-05 12:03:50 -0500

    Add docstrings to gpg.functions

--- in_toto/gpg/functions.py
+++ in_toto/gpg/functions.py
@@ -25,9 +25,34 @@ from in_toto.gpg.common import parse_signature_packet, parse_pubkey_packet
 
 import securesystemslib.formats
 
-# if None is used, then the keyid is not passed down and the signature is
-# performed with the default keyid
-def gpg_sign_object(content, keyid = None, homedir = None):
+
+def gpg_sign_object(content, keyid=None, homedir=None):
+  """
+  <Purpose>
+    Calls gpg command line utility to sign the passed content with the key
+    identified by the passed keyid from the gpg keyring at the passed homedir.
+
+  <Arguments>
+    content:
+            The content to be signed. (bytes)
+
+    keyid: (optional)
+            The keyid of the gpg signing keyid. If not passed the default
+            key in the keyring is used.
+            Note: On not fully supported gpg versions the keyid must be passed.
+
+    homedir: (optional)
+            Path to the gpg keyring. If not passed the default keyring is used.
+
+  <Exceptions>
+    None.
+
+  <Side Effects>
+    None.
+
+  <Returns>
+    The created signature in the format: gpg.formats.SIGNATURE_SCHEMA.
+  """
 
   keyarg = ""
   if keyid:
@@ -58,12 +83,62 @@ def gpg_sign_object(content, keyid = None, homedir = None):
 
 
 def gpg_verify_signature(signature_object, pubkey_info, content):
+  """
+  <Purpose>
+    Verifies the passed signature against the passed content using the
+    passed public key.
+
+    The function selects the appropriate verification algorithm (rsa or dsa)
+    based on the "type" field in the passed public key object.
+
+  <Arguments>
+    signature_object:
+            A signature object in the format: gpg.formats.SIGNATURE_SCHEMA
+
+    pubkey_info:
+            A public key object in the format: gpg.formats.PUBKEY_SCHEMA
+
+    content:
+            The content to be verified. (bytes)
+
+  <Exceptions>
+    None.
+
+  <Side Effects>
+    None.
+
+  <Returns>
+    True if signature verification passes, False otherwise.
+
+  """
   handler = SIGNATURE_HANDLERS[pubkey_info['type']]
   return handler.gpg_verify_signature(signature_object, pubkey_info, content)
 
 
-def gpg_export_pubkey(keyid, homedir = None):
+def gpg_export_pubkey(keyid, homedir=None):
+  """
+  <Purpose>
+    Calls gpg command line utility to export the gpg public key identified by
+    the passed keyid from the gpg keyring at the passed homedir in a format
+    suitable for in-toto.
 
+  <Arguments>
+    keyid:
+            The GPG keyid in format: securesystemslib.formats.KEYID_SCHEMA
+
+    homedir: (optional)
+            Path to the gpg keyring. If not passed the default keyring is used.
+
+  <Exceptions>
+    Value Error if the keyid does not match the required format
+
+  <Side Effects>
+    None.
+
+  <Returns>
+    The exported public key object in the format: gpg.formats.PUBKEY_SCHEMA
+
+  """
   if not securesystemslib.formats.KEYID_SCHEMA.matches(keyid):
     # FIXME: probably needs smarter parsing of what a valid keyid is so as to
     # not export more than one pubkey packet.
