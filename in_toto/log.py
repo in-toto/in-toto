145460abbf0f8ea9f6de5a5b347dacb1744dbaeb
Michael Kotlyar <mdk369@nyu.edu>
2017-12-10 13:30:21 -0500

    Added support for colorizing log output

--- in_toto/log.py
+++ in_toto/log.py
@@ -17,29 +17,46 @@
 
 import logging
 import in_toto.settings
+import platform
 
 logging.basicConfig(level=in_toto.settings.LOG_LEVEL, format='%(message)s')
 
 def debug(msg):
   """ Verbose debugging """
-  logging.debug("DEBUG: {}".format(msg))
+  logging.debug("DEBUG: {}".format(color_code(msg, 10)))
 
 def info(msg):
   """Verbose user feedback. """
-  logging.info("{}".format(msg))
+  logging.info("INFO: {}".format(color_code(msg, 20)))
 
 def warn(msg):
   """Verbose user warning. """
-  logging.warn("WARNING: {}".format(msg))
+  logging.warn("WARNING: {}".format(color_code(msg, 30)))
 
 def error(msg):
   """Prints unexpected errors """
-  logging.error("ERROR: {}".format(msg))
+  logging.error("ERROR: {}".format(color_code(msg, 40)))
 
 def pass_verification(msg):
   """Prints passing verification routines. """
-  logging.critical("PASSING: {}".format(msg))
+  logging.critical("PASSING: {}".format(color_code(msg, 50)))
 
 def fail_verification(msg):
   """Prints failing verification. """
-  logging.critical("FAILING: {}".format(msg))
+  logging.critical("FAILING: {}".format(color_code(msg, 50)))
+
+def color_code(msg, lvl):
+  """ Colorizing output for operating systems that support ANSI codes """
+  if in_toto.settings.COLOR:
+    level = logging.getLevelName(lvl)
+    levelGenerator = { "CRITICAL" : "\x1b[31m", # red
+                       "ERROR" : "\x1b[31m", #  red
+                       "WARNING" : "\x1b[33m", # yellow
+                       "INFO" : "\x1b[32m",  # green
+                       "DEBUG" : "\x1b[35m" } # magenta 
+    #FIXME: Add support for Windows OS (Windows has no support for ANSI codes)
+    if platform.system().lower() is not "windows":
+      for key, value in levelGenerator.iteritems():
+        if level in key:
+          return "{}{}\x1b[0m".format(value, msg)
+  return msg
