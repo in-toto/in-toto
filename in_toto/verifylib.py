430effb5d6e2e68426ed15d6b06c76a62cf01b29
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-01-16 18:12:20 -0500

    Verify verifylib link signature verification
    This commit combines 'verify_link_signatures' and
    'verify_all_steps_signatures' into the function
    'verify_link_signature_thresholds', which verifies signatures of
    the passed links according to the layouts requirements (thresholds)
    and authorizations (pubkeys).
    
    If does not immediately fail if a passed link is not authorized or
    has an invalid signature, as long as there are enough (threshold)
    valid and authorized links in the end.
    
    In the latter case the function returns an updated link dictionary
    containing only the valid and authorized links.

--- in_toto/verifylib.py
+++ in_toto/verifylib.py
@@ -252,46 +252,26 @@ def verify_layout_signatures(layout_metablock, keys_dict):
       if the any empty verification key dictionary was passed, or
       if any of the passed verification keys fails to verify a signature.
 
-def verify_link_signatures(link, keys_dict):
   """
-  <Purpose>
-    Iteratively verifies all signatures of a Link object using the passed
-    keys.
-
-  <Arguments>
-    link:
-            Metablock object (containing a Link) whose signatures are verified.
-
-    keys_dict:
-            A dictionary of keys to verify the signatures conformant with
-            securesystemslib.formats.KEYDICT_SCHEMA.
-
-  <Exceptions>
-    Raises an exception if a needed key can not be found in the passed
-    keys_dict or if a verification fails.
-    TBA (see https://github.com/in-toto/in-toto/issues/6)
   in_toto.formats.ANY_VERIFY_KEY_DICT_SCHEMA.check_match(keys_dict)
 
-  <Side Effects>
-    Verifies cryptographic Link signatures.
   # Fail if an empty verification key dictionary was passed
   if len(keys_dict) < 1:
     raise SignatureVerificationError("Layout signature verification"
         " requires at least one key.")
 
-  """
-  link.verify_signatures(keys_dict)
   # Fail if any of the passed keys can't verify a signature on the Layout
   for junk, verify_key in six.iteritems(keys_dict):
     layout_metablock.verify_signature(verify_key)
 
 
-def verify_all_steps_signatures(layout, chain_link_dict):
+def verify_link_signature_thresholds(layout, chain_link_dict):
   """
   <Purpose>
-    Extracts the Steps of a passed Layout and iteratively verifies the
-    the signatures of the Link object(s) related to each Step by the name field.
-    The public keys used for verification are also extracted from the Layout.
+    Verify that for each step of the layout there are at least `threshold`
+    links, signed by different authorized functionaries and return the chain
+    link dictionary containing only authorized links whose signatures
+    were successfully verified.
 
   <Arguments>
     layout:
@@ -309,34 +289,68 @@ def verify_all_steps_signatures(layout, chain_link_dict):
             }
 
   <Exceptions>
-    Raises an exception if a needed key can not be found in the passed
-    keys_dict or if a verification fails.
-    TBA (see https://github.com/in-toto/in-toto/issues/6)
+    ThresholdVerificationError
+            If any of the steps of the passed layout does not have enough
+            (`step.threshold`) links signed by different authorized
+            functionaries.
+
+  <Returns>
+    A chain_link_dict containing only links with valid signatures created by
+    authorized functionaries.
 
   """
+
+  verfied_chain_link_dict = {}
+  # Check signatures on passed links, if they are valid and authorized, but
+  # don't fail yet, instead add authorized links with passing signatures
+  # to a `verfied_chain_link_dict` and check later if the threshold
+  # requirements are fulfilled. That is, we don't care if there are a few
+  # bad links, as long as we have enough good links. Only the good links will
+  # be considered for further final product verification.
   for step in layout.steps:
-    # Find the according link for this step
-    key_link_dict = chain_link_dict[step.name]
+    # Contains all links corresponding to a step with successfully verified
+    # signatures, authorized to perform the step
+    verified_key_link_dict = {}
 
-    for keyid, link in six.iteritems(key_link_dict):
-      keys_dict = {}
+    # Iterate over links corresponding to a step
+    for keyid, link in six.iteritems(chain_link_dict.get(step.name, {})):
 
-      # Create the dictionary of keys for this step
-      # Only one key with the matching keyid in the
-      # filename is added to the dictionary which ensures
-      # that the link has been signed by that key
-      if keyid in step.pubkeys:
-        keys_dict[keyid] = layout.keys[keyid]
+      # Skip unauthorized links
+      if keyid not in step.pubkeys:
+        log.info("Skipping link. Keyid '{0}' is not authorized to sign links"
+            " for step '{1}'".format(keyid, step.name))
+        continue
+
+      # Verify signature and skip invalidly signed links
+      try:
+        verify_key = layout.keys.get(keyid)
+        link.verify_signature(verify_key)
+
+      except SignatureVerificationError:
+        log.info("Skipping link. Broken link signature with keyid '{0}'"
+            " for step '{1}'".format(keyid, step.name))
+
       else:
-        raise AuthorizationError("Unauthorized Key! '{0}'".format(keyid))
+        # Good link: The signature is valid and the signer was authorized
+        verified_key_link_dict[keyid] = link
 
-      log.info("Verifying signature(s) for '{0}'...".format(
-          in_toto.models.link.FILENAME_FORMAT.format(step_name=step.name,
-              keyid=keyid)))
+    # Add all good links for a step to the dictionary of links for all steps
+    verfied_chain_link_dict[step.name] = verified_key_link_dict
 
-      # Verify link metadata file's signatures
-      verify_link_signatures(link, keys_dict)
+  # For each step, verify that we have enough validly signed links signed by
+  # different authorized functionaries.
+  # TODO: To guarantee that links are signed by different functionaries
+  # we rely on the layout to not carry duplicate verification keys under
+  # different dictionary keys, e.g. {keyid1: KEY1, keyid2: KEY1}
+  # Maybe we should add such a check to the layout validation? Or here?
+  for step in layout.steps:
+    valid_authorized_links_cnt = len(verfied_chain_link_dict[step.name])
+    if valid_authorized_links_cnt < step.threshold:
+      raise ThresholdVerificationError("Step requires at least '{}' links"
+          " validly signed by different authorized functionaries. Only"
+          " found '{}'".format(step.threshold, valid_authorized_links_cnt))
 
+  return verfied_chain_link_dict
 
 def verify_command_alignment(command, expected_command):
   """
@@ -1335,7 +1349,7 @@ def in_toto_verify(layout, layout_key_dict):
   chain_link_dict = load_links_for_layout(layout)
 
   log.info("Verifying link metadata signatures...")
-  verify_all_steps_signatures(layout, chain_link_dict)
+  chain_link_dict = verify_link_signature_thresholds(layout, chain_link_dict)
 
   log.info("Verifying sublayouts...")
   chain_link_dict = verify_sublayouts(layout, chain_link_dict)
