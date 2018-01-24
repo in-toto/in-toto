38ef5c378ca7147bad1cc1afb2920f891492685f
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-01-24 16:44:55 -0500

    Add pack_rule functions to artifact_rules
    Steps and Inspections carry artifact rules in list forms. To
    parse a rule in list form into a dictionary, which is easier
    to work with, we use unpack_rule.
    
    This commit adds a pack_rule function to create the rules based
    on the fields in the rule_data function and a shortcut to
    directly use the rule_data dictionary returned from unpack_rule
    to get back to the rule in list format, e.g.:
    pack_rule_data(unpack_rule(rule)).
    
    Additionally this commit adds shortcuts for create, delete, modify,
    allow and disallow rules.
    
    This functions can be used for layout creation.

--- in_toto/artifact_rules.py
+++ in_toto/artifact_rules.py
@@ -21,6 +21,10 @@ import in_toto.formats
 import securesystemslib.exceptions
 import securesystemslib.formats
 
+GENERIC_RULES = {"create", "modify", "delete", "allow", "disallow",}
+COMPLEX_RULES = {"match",}
+ALL_RULES = GENERIC_RULES | COMPLEX_RULES
+
 def unpack_rule(rule):
   """
   Parses the rule and extracts and returns the necessary data to apply the
@@ -62,10 +66,6 @@ def unpack_rule(rule):
     }
 
   """
-
-  GENERIC_RULES = {"create", "modify", "delete", "allow", "disallow",}
-  COMPLEX_RULES = {"match",}
-  ALL_RULES = GENERIC_RULES | COMPLEX_RULES
   in_toto.formats.LIST_OF_ANY_STRING_SCHEMA.check_match(rule)
 
   # Create all lower rule copy to case insensitively parse out tokens whose
@@ -162,3 +162,126 @@ def unpack_rule(rule):
       "dest_type" : dest_type,
       "dest_name": dest_name
     }
+
+
+def pack_rule(rule_type, pattern, source_prefix=None, dest_type=None,
+      dest_prefix=None, dest_name=None):
+  """
+  <Purpose>
+    Create an artifact rule in the passed arguments and return it as list
+    as it is stored in a step's or inspection's expected_material or
+    expected_product field.
+
+  <Arguments>
+    rule_type:
+            One of "MATCH", "CREATE", "DELETE", MODIFY, ALLOW, DISALLOW
+            (case insensitive).
+
+    pattern:
+            A glob pattern to match artifact paths.
+
+    source_prefix: (only for "MATCH" rules)
+            A prefix for 'pattern' to match artifacts reported by the link
+            corresponding to the step that contains the rule.
+
+    dest_type: (only for "MATCH" rules)
+            One of "MATERIALS" or "PRODUCTS" (case insensitive) to specify
+            if materials or products of the link corresponding to the step
+            identified by 'dest_name' should be matched.
+
+    dest_prefix: (only for "MATCH" rules)
+            A prefix for 'pattern' to match artifacts reported by the link
+            corresponding to the step identified by 'dest_name'.
+
+    dest_name: (only for "MATCH" rules)
+            The name of the step whose corresponding link is used to match
+            artifacts.
+
+
+  <Exceptions>
+    securesystemslib.exceptions.FormatError
+            if any of the arguments is malformed.
+
+
+  <Returns>
+    One of the following rule formats in as a list
+      MATCH <pattern> [IN <source-path-prefix>] WITH (MATERIALS|PRODUCTS)
+          [IN <destination-path-prefix>] FROM <step>,
+      CREATE <pattern>,
+      DELETE <pattern>,
+      MODIFY <pattern>,
+      ALLOW <pattern>,
+      DISALLOW <pattern>
+
+  """
+  in_toto.formats.ANY_STRING_SCHEMA.check_match(rule_type)
+  in_toto.formats.ANY_STRING_SCHEMA.check_match(pattern)
+
+  if rule_type.lower() not in ALL_RULES:
+    raise securesystemslib.exceptions.FormatError("'{0}' is not a valid 'type'."
+        " Rule type must be one of:  {1} (case insensitive)."
+        .format(rule_type, ", ".join(ALL_RULES)))
+
+  if rule_type.upper() == "MATCH":
+    if (not in_toto.formats.ANY_STRING_SCHEMA.matches(dest_type) or not
+        (dest_type.lower() == "materials" or dest_type.lower() == "products")):
+      raise securesystemslib.exceptions.FormatError("'{}' is not a valid"
+          " 'dest_type'. Rules of type 'MATCH' require a destination type of"
+          " either 'MATERIALS' or 'PRODUCTS' (case insensitive)."
+          .format(dest_type))
+
+    if not (in_toto.formats.ANY_STRING_SCHEMA.matches(dest_name) and dest_name):
+      raise securesystemslib.exceptions.FormatError("'{}' is not a valid"
+          " 'dest_name'. Rules of type 'MATCH' require a step name as a"
+          " destination name.".format(dest_name))
+
+    # Construct rule
+    rule = ["MATCH", pattern]
+
+    if source_prefix:
+      in_toto.formats.ANY_STRING_SCHEMA.check_match(source_prefix)
+      rule += ["IN", source_prefix]
+
+    rule += ["WITH", dest_type.upper()]
+
+    if dest_prefix:
+      in_toto.formats.ANY_STRING_SCHEMA.check_match(dest_prefix)
+      rule += ["IN", dest_prefix]
+
+    rule += ["FROM", dest_name]
+
+  else:
+    rule = [rule_type.upper(), pattern]
+
+  return rule
+
+
+def pack_rule_data(rule_data):
+  """Shortcut for 'pack_rule' to pack a rule based on a rule_data dictionary
+  as returned by 'unpack_rule'. """
+  return pack_rule(**rule_data)
+
+
+def pack_create_rule(pattern):
+  """Shortcut for 'pack_rule' to pack a CREATE rule. """
+  return pack_rule("CREATE", pattern)
+
+
+def pack_delete_rule(pattern):
+  """Shortcut for 'pack_rule' to pack a DELETE rule. """
+  return pack_rule("DELETE", pattern)
+
+
+def pack_modify_rule(pattern):
+  """Shortcut for 'pack_rule' to pack a MODIFY rule. """
+  return pack_rule("MODIFY", pattern)
+
+
+def pack_allow_rule(pattern):
+  """Shortcut for 'pack_rule' to pack an ALLOW rule. """
+  return pack_rule("ALLOW", pattern)
+
+
+def pack_disallow_rule(pattern):
+  """Shortcut for 'pack_rule' to pack a DISALLOW rule. """
+  return pack_rule("DISALLOW", pattern)
