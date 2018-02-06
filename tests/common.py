1c2e3f691d75d0747edc6dd9e4fecae619e74d7c
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-02-06 15:48:53 -0500

    Add common tests module with CliTestCase class
    CliTestCase is a subclass of unittest.TestCase and provides a
    method that patches sys.argv (command line args) and expects a
    SystemExit exception to be raised, which is handy for testing
    the command line interfaces' argparsing methods.

---
+++ tests/common.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+"""
+<Program Name>
+  common.py
+
+<Author>
+  Lukas Puehringer <lukas.puehringer@nyu.edu>
+
+<Started>
+  Feb 6, 2018
+
+<Copyright>
+  See LICENSE for licensing information.
+
+<Purpose>
+  Common code for in-toto unittests, import like so:
+  `import tests.common`
+
+  Tests importing this module, should be run from the project root, e.g.:
+  `python tests/test_in_toto_run.py`
+  or using the aggregator script (preferred way):
+  `python tests/runtests.py`.
+
+"""
+import os
+import sys
+import inspect
+
+import unittest
+from mock import patch
+
+
+class CliTestCase(unittest.TestCase):
+  """TestCase subclass providing a test helper that patches sys.argv with
+  passed arguments and asserts a SystemExit with a return code equal
+  to the passed status argument.
+
+  Subclasses of CliTestCase require a class variable that stores the main
+  function of the cli tool to test as staticmethod, e.g.:
+
+  ```
+  import tests.common
+  from in_toto.in_toto_run import main as in_toto_run_main
+
+  class TestInTotoRunTool(tests.common.CliTestCase):
+    cli_main_func = staticmethod(in_toto_run_main)
+    ...
+
+  ```
+  """
+  cli_main_func = None
+
+  def __init__(self, *args, **kwargs):
+    """Constructor that checks for the presence of a callable cli_main_func
+    class variable. And stores the filename of the module containing that
+    function, to be used as first argument when patching sys.argv in
+    self.assert_cli_sys_exit.
+    """
+    if not callable(self.cli_main_func):
+      raise Exception("Subclasses of `CliTestCase` need to assign the main"
+          " function of the cli tool to test using `staticmethod()`: {}"
+          .format(self.__class__.__name__))
+
+    file_path = inspect.getmodule(self.cli_main_func).__file__
+    self.file_name = os.path.basename(file_path)
+
+    super(CliTestCase, self).__init__(*args, **kwargs)
+
+
+  def assert_cli_sys_exit(self, cli_args, status):
+    """Test helper to mock command line call and assert return value.
+    The passed args does not need to contain the command line tool's name.
+    This is assessed from  `self.cli_main_func`
+    """
+    with patch.object(sys, "argv", [self.file_name]
+        + cli_args), self.assertRaises(SystemExit) as raise_ctx:
+      self.cli_main_func()
+
+    self.assertEqual(raise_ctx.exception.code, status)
