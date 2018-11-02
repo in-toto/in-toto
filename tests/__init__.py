aa2b303354a605ca7620f2dfca578dd30f362d3c
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-11-02 14:14:45 +0100

    Use custom logging.StreamHandler for testing
    We used to suppress logging by replacing in_toto's base logger's
    StreamHandler with a NullHandler for tests. While this eliminated
    test log inundation, it was not helpful to troubleshoot failing
    tests.
    
    This commit implements a custom streamhandler for tests, in order
    to allow unittest stdout/stderr buffering and only show output in
    case a test fail.
    
    Inspired by https://stackoverflow.com/a/33271004

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,29 @@
-import in_toto
+import sys
 import logging
+import in_toto
 
-# Override in-toto logger default StreamHandler to prevent test log inundation
-in_toto.log.logger.handlers = [logging.NullHandler()]
+class CapturableStreamHandler(logging.StreamHandler):
+  """Override logging.StreamHandler's stream property to always write log
+  output to `sys.stderr` available at the time of logging.
+  """
+  @property
+  def stream(self):
+    """Always use currently available sys.stderr. """
+    return sys.stderr
+
+  @stream.setter
+  def stream(self, value):
+    """Disable setting stream. """
+    pass
+
+# Python `unittest` is configured to buffer output to `sys.stdout/sys.stderr`
+# (see `TextTestRunner` in `tests/runtests.py`) and only show it in case a test
+# fails. Python `unittest` buffers output by overriding `sys.stdout/sys.stderr`
+# before running tests, hence we need to log to that overridden `sys.stderr`,
+# which we do by using a custom StreamHandler.
+handler = CapturableStreamHandler()
+# We also use a verbose logging level and format
+formatter = logging.Formatter(in_toto.log._FORMAT_DEBUG)
+handler.setFormatter(formatter)
+in_toto.log.logger.handlers = [handler]
+in_toto.log.logger.setLevel(logging.DEBUG)
