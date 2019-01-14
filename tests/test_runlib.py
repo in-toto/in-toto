59f26195e790e01995ab0ec384ad5c5ed0ef719c
Aditya Saky <aditya@saky.in>
2019-01-14 15:46:57 -0500

    (WIP) Rename basic test for lstrip paths

--- tests/test_runlib.py
+++ tests/test_runlib.py
@@ -189,7 +189,7 @@ class TestRecordArtifactsAsDict(unittest.TestCase):
     os.chdir(self.test_dir)
 
 
-  def test_lstrip_paths(self):
+  def test_lstrip_paths_valid_prefix(self):
     lstrip_paths = "subdir/subsubdir"
     expected_artifacts = sorted(["bar", "foo", "subdir/foosub1",
       "subdir/foosub2", "foosubsub"])
