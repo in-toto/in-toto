cf2dd3fa623028f62f399d3e12144539eab04719
Aditya Saky <aditya@saky.in>
2019-01-21 12:58:51 -0500

    Treat test case prefixes as single item lists

--- tests/test_runlib.py
+++ tests/test_runlib.py
@@ -192,7 +192,7 @@ class TestRecordArtifactsAsDict(unittest.TestCase):
 
 
   def test_lstrip_paths_valid_prefix_directory(self):
-    lstrip_paths = "subdir/subsubdir/"
+    lstrip_paths = ["subdir/subsubdir/"]
     expected_artifacts = sorted(["bar", "foo", "subdir/foosub1",
         "subdir/foosub2", "foosubsub"])
     artifacts_dict = record_artifacts_as_dict(["."],
@@ -202,7 +202,7 @@ class TestRecordArtifactsAsDict(unittest.TestCase):
 
 
   def test_lstrip_paths_invalid_prefix_directory(self):
-    lstrip_paths = "not/a/directory/"
+    lstrip_paths = ["not/a/directory/"]
     expected_artifacts = sorted(["bar", "foo", "subdir/foosub1",
                                  "subdir/foosub2", "subdir/subsubdir/foosubsub"])
     artifacts_dict = record_artifacts_as_dict(["."],
@@ -212,7 +212,7 @@ class TestRecordArtifactsAsDict(unittest.TestCase):
 
 
   def test_lstrip_paths_valid_prefix_file(self):
-    lstrip_paths = "subdir/subsubdir/"
+    lstrip_paths = ["subdir/subsubdir/"]
     expected_artifacts = sorted(["foosubsub"])
     artifacts_dict = record_artifacts_as_dict(["./subdir/subsubdir/foosubsub"],
         lstrip_paths=lstrip_paths)
@@ -230,7 +230,7 @@ class TestRecordArtifactsAsDict(unittest.TestCase):
         fp.write(path)
 
       # Attempt to left strip the path now that the file has been created
-      lstrip_paths = "ಠ/"
+      lstrip_paths = ["ಠ/"]
       expected_artifacts = sorted(["foobar"])
       artifacts_dict = record_artifacts_as_dict(["./ಠ/"],
           lstrip_paths=lstrip_paths)
@@ -251,7 +251,7 @@ class TestRecordArtifactsAsDict(unittest.TestCase):
         fp.write(path)
 
       # Attempt to left strip the path now that the file has been created
-      lstrip_paths = "ಠ/"
+      lstrip_paths = ["ಠ/"]
       expected_artifacts = sorted(["foobar"])
       artifacts_dict = record_artifacts_as_dict(["./ಠ/"],
                                                 lstrip_paths=lstrip_paths)
