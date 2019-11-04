ed8233bc34835cc5d045e185936219cfab024428
Santiago Torres <santiago@archlinux.org>
2019-11-04 16:08:40 -0500

    doc:conf: tell rtd to use index.rst as toplevel
    Signed-off-by: Santiago Torres <santiago@archlinux.org>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,6 +29,10 @@ release = in_toto.__version__
 
 # -- General configuration ---------------------------------------------------
 
+# The master toctree document.
+master_doc = 'index'
+
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
