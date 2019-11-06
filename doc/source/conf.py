58ce808ab60eb0b30b6b0230b4b05025898869d6
Santiago Torres <santiago@archlinux.org>
2019-11-06 10:26:02 -0500

    docs: remove path insertion for in_toto.version
    Signed-off-by: Santiago Torres <santiago@archlinux.org>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+# NOTE: if you want to build the docs without installing in-toto, uncomment the
+# three lines below
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
 import in_toto
 
