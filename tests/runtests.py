99a336d2c7d39fbea81a8f03ea28c19653a8f10a
Joshua Lock <jlock@vmware.com>
2020-02-28 11:59:06 +0000

    Use constant from securesystemslib to detect GPG in tests
    Use the new constant in securesystemslib 0.14.1 to check for the
    presence of GPG while running tests.
    
    Signed-off-by: Joshua Lock <jlock@vmware.com>

--- tests/runtests.py
+++ tests/runtests.py
@@ -20,29 +20,6 @@
 
 from unittest import defaultTestLoader, TextTestRunner
 import sys
-import os
-import subprocess
-
-def check_usable_gpg():
-  """Set `TEST_SKIP_GPG` environment variable if neither gpg2 nor gpg is
-  available.
-
-  """
-  os.environ["TEST_SKIP_GPG"] = "1"
-  for gpg in ["gpg2", "gpg"]:
-    try:
-      subprocess.check_call([gpg, "--version"])
-
-    except OSError:
-      pass
-
-    else:
-      # If one of the two exists, we can unset the skip envvar and ...
-      os.environ.pop("TEST_SKIP_GPG", None)
-      # ... abort the availability check.:
-      break
-
-check_usable_gpg()
 
 suite = defaultTestLoader.discover(start_dir=".")
 result = TextTestRunner(verbosity=2, buffer=True).run(suite)
