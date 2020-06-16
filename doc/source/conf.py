86e87beb15f1da5b22c35bef959f762e105db5d2
Lukas Puehringer <lukas.puehringer@nyu.edu>
2020-06-16 16:54:01 +0200

    Update misc sphinx doc config
    - Switch from alabaster to sphinx_rtd_theme
    - Add napoleon sphinx extension to parse Google-style docstrings
    - Add todo sphinx extension (for utils docs)
    - Comment out currently unused html_static_path that triggered a
      warning
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>
    Signed-off-by: cindykimxp <cindykim100@gmail.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,7 @@
 # three lines below
 import os
 import sys
+import sphinx_rtd_theme
 # sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
 sys.path.append(os.path.abspath("./_ext"))
@@ -40,7 +41,8 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxarg.ext', 'argparse_epilog']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon', 'sphinxarg.ext',
+    'argparse_epilog', 'recommonmark', 'sphinx.ext.todo', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -56,9 +58,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+
+todo_include_todos = True
