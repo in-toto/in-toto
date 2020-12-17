cf3f0800806ade571edf8bb57f22d363087a9ee7
Lukas Puehringer <lukas.puehringer@nyu.edu>
2020-12-17 17:49:48 +0100

    Add white horiztonal in-toto logo for RTD
    Horizontal looks nicer than stacked and white looks nicer than
    red/blue for the currently used RTD theme.
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -59,6 +59,10 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+  'logo_only': True
+}
+html_logo = 'in-toto-horizontal-white.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
