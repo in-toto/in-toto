bf1be7cfe1c66269bc8ec23ceba6f92dfc62e9e3
Pradyumna Krishna <git@onpy.in>
2022-10-26 11:27:11 +0530

    Add in-toto Envelope test case
    Adds in-toto envelope payload parsing and envelope creation
    test cases.
    
    Signed-off-by: Pradyumna Krishna <git@onpy.in>

---
+++ tests/models/test_envelope.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+# Copyright New York University and the in-toto contributors
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+<Program Name>
+  test_envelope.py
+
+<Author>
+  Lukas Puehringer <lukas.puehringer@nyu.edu>
+
+<Started>
+  Oct 25, 2022
+
+<Copyright>
+  See LICENSE for licensing information.
+
+<Purpose>
+  Test in_toto.models.metadata.Envelope class methods.
+
+"""
+
+import os
+import shutil
+import unittest
+
+from in_toto.models.metadata import Envelope
+from in_toto.models.layout import Layout
+from in_toto.models.link import Link
+
+from tests.common import TmpDirMixin
+
+class TestMetablockValidator(unittest.TestCase, TmpDirMixin):
+  """Test in_toto.models.metadata.Envelope methods. """
+
+  def test_create_envelope(self):
+    """Test DSSE envelope creation from Link or Layout."""
+
+    # Create DSSE Envelope from empty Link.
+    link = Link()
+    env = Envelope.from_signable(link)
+
+    # Verify links.
+    self.assertEqual(env.get_payload(), link)
+
+    # Create DSSE Envelope from empty Layout.
+    layout = Layout()
+    env = Envelope.from_signable(layout)
+
+    # Verify layouts.
+    self.assertEqual(env.get_payload(), layout)
+
+  def test_load_envelope(self):
+    """Test loading and parsing of in-toto envelope."""
+
+    # Demo DSSE Metadata Files
+    demo_dsse_files = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "../demo_dsse_files")
+
+    self.set_up_test_dir()
+
+    # Copy demo DSSE files to temp dir
+    for file_path in os.listdir(demo_dsse_files):
+      shutil.copy(os.path.join(demo_dsse_files, file_path), self.test_dir)
+
+    layout_path = "demo.layout.template"
+    link_path = "package.2f89b927.link"
+
+    # Load layout metadata.
+    env = Envelope.from_file(layout_path)
+    layout = env.get_payload()
+
+    self.assertIsInstance(layout, Layout)
+
+    # Load link metadata.
+    env = Envelope.from_file(link_path)
+    link = env.get_payload()
+
+    self.assertIsInstance(link, Link)
+
+
+if __name__ == "__main__":
+  unittest.main()
