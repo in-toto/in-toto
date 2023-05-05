1288bfced6dd384e704c3d3797ce2aaa2234048b
Aditya Sirish <aditya@saky.in>
2023-05-05 13:24:16 -0400

    Add dirHash resolver
    The in-toto attestation specification describes how a directory can be
    hashed recursively with all of its contents. It implements the same
    hashing routine used by Go Modules, though hashes are encoded in
    hexadecimal rather than base64.
    
    This commit adds DirectoryResolver that can be invoked from
    record_artifacts_as_dict. Under the hood, it uses FileResolver to
    recursively hash every file in the directory. DirectoryResolver accepts
    exclude_patterns, follow_symlink_dirs, normalize_line_endings, and
    lstip_paths. Of these, the first three are passed on to FileResolver.
    Further, for each directory being hashed, DirectoryResolver sets the
    path of the directory as the base_path for the corresponding
    FileResolver object.
    
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- in_toto/resolver/_resolver.py
+++ in_toto/resolver/_resolver.py
@@ -1,12 +1,15 @@
-"""Resolver interface and default file resolver implementation. """
+"""Resolver interface and implementations for files, OSTree, and directory
+artifacts."""
+import locale
 import logging
 import os
 from abc import ABCMeta, abstractmethod
+from functools import cmp_to_key
 from itertools import combinations
 from os.path import exists, isdir, isfile, join, normpath
 
 from pathspec import GitIgnoreSpec
-from securesystemslib.hash import digest_filename
+from securesystemslib.hash import digest, digest_filename
 
 from in_toto.exceptions import PrefixError
 
@@ -94,12 +97,12 @@ class FileResolver(Resolver):
 
     def _hash(self, path):
         """Helper to generate hash dictionary for path."""
-        digest = digest_filename(
+        digest_obj = digest_filename(
             path,
             algorithm=_HASH_ALGORITHM,
             normalize_line_endings=self._normalize_line_endings,
         )
-        return {_HASH_ALGORITHM: digest.hexdigest()}
+        return {_HASH_ALGORITHM: digest_obj.hexdigest()}
 
     def _mangle(self, path, existing_paths, scheme_prefix):
         """Helper for path mangling."""
@@ -238,9 +241,12 @@ class OSTreeResolver(Resolver):
             "objects", ref_contents[:2], f"{ref_contents[2:]}.commit"
         )
 
-        digest = digest_filename(object_path, algorithm=self._HASH_ALGORITHM)
+        digest_obj = digest_filename(
+            object_path,
+            algorithm=self._HASH_ALGORITHM,
+        )
 
-        return {self._HASH_ALGORITHM: digest.hexdigest()}
+        return {self._HASH_ALGORITHM: digest_obj.hexdigest()}
 
     def hash_artifacts(self, uris):
         hashes = {}
@@ -257,5 +263,118 @@ class OSTreeResolver(Resolver):
         # Change back to original current working dir
         if self._base_path:
             os.chdir(original_cwd)
+
+        return hashes
+
+
+class DirectoryResolver(Resolver):
+    """Directory resolver implementation.
+
+    Implementation of the following shell command for some directory URI:
+
+    find . -type f | cut -c3- | LC_ALL=C sort | xargs -r sha256sum | sha256sum |
+        cut -f1 -d' '
+
+    `exclude_patterns` are not applied on the directory paths passed to
+    `hash_artifacts`, but on the files inside the directory. Similarly,
+    `follow_symlink_dirs` and `normalize_line_endings` are used on
+    subdirectories and files inside the directories passed to `hash_artifacts`.
+
+    """
+
+    SCHEME = "dir"
+
+    def __init__(
+        self,
+        exclude_patterns=None,
+        follow_symlink_dirs=False,
+        normalize_line_endings=False,
+        lstrip_paths=None,
+    ):
+        if not exclude_patterns:
+            exclude_patterns = []
+
+        if not lstrip_paths:
+            lstrip_paths = []
+
+        self._exclude_patterns = exclude_patterns
+        self._follow_symlink_dirs = follow_symlink_dirs
+        self._normalize_line_endings = normalize_line_endings
+        self._lstrip_paths = lstrip_paths
+
+    def _strip_scheme_prefix(self, path):
+        """Helper to strip file resolver scheme prefix from path."""
+
+        return path[len(self.SCHEME + ":") :]
+
+    def _mangle(self, path, existing_paths):
+        """Helper for path mangling."""
+
+        # Normalize slashes for cross-platform metadata consistency
+        path = path.replace("\\", "/")
+
+        # Left-strip names using configured path prefixes
+        for lstrip_path in self._lstrip_paths:
+            if path.startswith(lstrip_path):
+                path = path[len(lstrip_path) :]
+                break
+
+        # Prepend passed scheme prefix
+        # We do this first because the entries in existing_paths have the scheme
+        # affixed already.
+        path = self.SCHEME + ":" + path
+
+        # Fail if left-stripping above results in duplicates
+        if self._lstrip_paths and path in existing_paths:
+            raise PrefixError(
+                "Prefix selection has resulted in non unique dictionary key "
+                f"'{path}'"
+            )
+
+        return path
+
+    def _hash(self, file_hashes):
+        """Helper to correctly sort and hash every element."""
+
+        text_repr = ""
+        keys = list(file_hashes.keys())
+        if keys:
+            locale.setlocale(locale.LC_ALL, "C")
+            keys.sort(key=cmp_to_key(locale.strcoll))
+
+            text_repr = "\n".join(
+                [f"{file_hashes[k][_HASH_ALGORITHM]}  {k}" for k in keys]
+            )
+            text_repr += "\n"  # trailing new line
+
+        digest_obj = digest(_HASH_ALGORITHM)
+        digest_obj.update(text_repr.encode("utf-8"))
+
+        return {_HASH_ALGORITHM: digest_obj.hexdigest()}
+
+    def hash_artifacts(self, uris):
+        hashes = {}
+
+        for path in uris:
+            path = self._strip_scheme_prefix(path)
+
+            if not os.path.isdir(path):
+                raise ValueError(f"path '{path}' is not a directory")
+
+            file_resolver = FileResolver(
+                base_path=path,
+                exclude_patterns=self._exclude_patterns,
+                follow_symlink_dirs=self._follow_symlink_dirs,
+                normalize_line_endings=self._normalize_line_endings,
+            )
+
+            file_hashes = file_resolver.hash_artifacts(["."])
+            if not file_hashes:
+                logger.info(
+                    "path: %s has no files, recording empty dir...", path
+                )
+
+            name = self._mangle(path, hashes)
+            hashes[name] = self._hash(file_hashes)
 
         return hashes
